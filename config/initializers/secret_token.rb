7a01fc183a6de35a76c304bdf1a8502bd7fc10e8
alainlap <alainlaplume@gmail.com>
2013-12-02 14:10:55 -0500

    created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrowdFunder::Application.config.secret_key_base = '7acb3c3e45f45ed5170d227be2e3c5c4b13ae68bb0af5a836306194ce361ccad8fe0d20a39aca196127f2c7ecb61150bf87f52a3b65baf0b5ed9c7ce986fce82'
