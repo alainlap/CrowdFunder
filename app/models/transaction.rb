b13864dddf5214458b29329f3d35f8d4111ff077
makeready <shotsinthedark@gmail.com>
2013-12-03 12:45:12 -0500

    added qualified_tiers method to Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,12 @@ class Transaction < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   belongs_to :tier
+
+  def qualified_tiers(amount)
+    qualified = []
+    self.project.tiers.each do |tier|
+      qualified << tier.id if amount >= tier.threshold
+    end
+    qualified 
+  end
 end
