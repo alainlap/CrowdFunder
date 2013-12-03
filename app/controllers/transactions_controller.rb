e0fe61d6c4cb31465f3751839bc16504dea0cb8f
makeready <shotsinthedark@gmail.com>
2013-12-03 14:31:35 -0500

    removed unneccessary line from transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,6 @@
 class TransactionsController < ApplicationController
   def create
     project = Project.find(params[:project_id])
-    contributor = current_user
     transaction = project.transactions.build(transaction_params)
     transaction.user_id = current_user.user_id
     transaction.tier_id = params([:tier_id])
