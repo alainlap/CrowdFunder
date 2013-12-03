9baa907a539f362e820c925a57fede1e724dc7e1
makeready <shotsinthedark@gmail.com>
2013-12-03 14:29:45 -0500

    set up transaction-create method

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,14 @@
 class TransactionsController < ApplicationController
   def create
+    project = Project.find(params[:project_id])
+    contributor = current_user
+    transaction = project.transactions.build(transaction_params)
+    transaction.user_id = current_user.user_id
+    transaction.tier_id = params([:tier_id])
+    if transaction.save
+      redirect_to project_path(project.id)
+    else
+      redirect_to project_path(project.id)
+    end
   end
 end
