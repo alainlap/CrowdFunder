0a9eb5deccba6ac240f2303fbdfff5a171eacc7c
leopold <leowicht@gmail.com>
2013-12-03 15:24:46 -0500

    removed turbolinks and worked on project index

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,15 @@ Project.create(name: "Amazing Project Title",
           min_pledge: 100,
           website: "www.google.com" )
 
+Project.create(name: "Sweet Project Two",
+          goal: 73400,
+          creator_id: 1,
+          description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
+          img: "http://tennis-pronostics.com/sites/default/files/photo_gallery/dodig_8.jpg",
+          end_date: Date.new(2015,01,01),
+          min_pledge: 100,
+          website: "www.google.com" )
+
 Tier.create(
   project_id: 1,
   threshold: 100,
