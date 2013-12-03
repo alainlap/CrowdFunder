59e40cc491cb832e64c787ba026713b6a7912cbb
makeready <shotsinthedark@gmail.com>
2013-12-03 12:30:47 -0500

    set up basic projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,40 @@
 class ProjectsController < ApplicationController
 	skip_before_filter :require_login, only: [:index, :show]
 
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    if current_user
+      @transaction = @project.transactions.build
+    end
+  end
+
+  def new
+    @creator = current_user
+    @project = Project.new
+    10.times {@project.tiers.build}
+  end
+
+  def create
+    @creator = current_user
+    @project = @creator.projects.build(project_params) #sets creator_id properly??
+    if @project.save
+      redirect_to projects_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :sponsors, through: :transactions, class_name: "User", source: :user
   has_many :tiers
+  accepts_nested_attributes_for :tiers
 end
