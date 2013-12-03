af2214bcb8602d53c2532fa5d647da685a0ead15
alainlap <alainlaplume@gmail.com>
2013-12-03 15:31:55 -0500

    fixed activate form javascript on user show page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,27 +1,34 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	if ($(".activate-form").length) {
-// 		$(".activate-form").toggle(activateForm, deactivateForm);
-// 		disableForms();
-// 	}
+	if ($(".activate-form").length) {
+		$(".activate-form").on('click', toggleForm);
+		disableForms();
+	}
 
-// });
+});
 
-// function activateForm() {
-// 		console.log("Activating");
-// 		$(this).html("Cancel").addClass("active");
-// 		$(this).closest("form").find("input").prop("disabled", false);
-// 		$(this).closest("form").find("input[type='submit']").css("display","block");
-// }
+function toggleForm(e) {
+	var self = $(this);
+	
+	e.preventDefault();
+	e.stopPropagation();
 
-// function deactivateForm() {
-// 		console.log("Deactivating");
-// 		$(this).html("Edit").removeClass("active");
-// 		$(this).closest("form").find("input").prop("disabled", true);
-// 		$(this).closest("form").find("input[type='submit']").css("display","none");
-// }
+	if (self.hasClass("active")) {
+		console.log("Deactivating");
+		self.html("Edit").removeClass("active");
+		self.closest("form").find("input").prop("disabled", true);
+		self.closest("form").find("input[type='submit']").css("display","none");
+	} else {
+		console.log("Activating");
+		self.html("Cancel").addClass("active");
+		self.closest("form").find("input").prop("disabled", false);
+		self.closest("form").find("input[type='submit']").css("display","block");
+	};
+};
 
-// function disableForms() {
-// 	$("input").prop("disabled", true);
-// 	$("input[type='submit']").css("display", "none");
-// }
+
+function disableForms() {
+	$("input").prop("disabled", true);
+	$("input[type='submit']").css("display", "none");
+	return false;
+}
