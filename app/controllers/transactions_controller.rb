a12cd8f0585ee662a190aa1a30d17778bcf8bd4c
makeready <shotsinthedark@gmail.com>
2013-12-03 12:43:23 -0500

    added sum_raised and percentage_raised to Project

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,4 @@
 class TransactionsController < ApplicationController
+  def create
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,16 @@ class Project < ActiveRecord::Base
   has_many :sponsors, through: :transactions, class_name: "User", source: :user
   has_many :tiers
   accepts_nested_attributes_for :tiers
+
+  def sum_raised
+    total = 0
+    self.transactions.each do |pledge|
+      total += pledge.dollaramount
+    end
+    total
+  end
+
+  def percentage_raised
+    sum_raised / goal
+  end
 end
